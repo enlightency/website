d25d46f86b584a4a96ff16accb1291e10189b2da
Shengyou Fan <shengyoufan@gmail.com>
2019-04-07 17:41:21 +0800

    update content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,119 +2,69 @@
   <Layout>
     <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
       <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
-        <div class="leading-tight">Gridsome Portfolio Starter</div>
-        <div class="text-green-700 leading-tight">Powered by Vue.js &amp Tailwind 1.0</div>
+        <div class="leading-tight">澄煦顧問</div>
+        <div class="text-blue-700 leading-tight">提供技術品牌經營服務</div>
       </div>
       <div class="mt-8 sm:mt-0">
         <g-image src="../../static/developer.svg" alt="hero" class="mx-auto sm:mx-0" />
       </div>
     </div> <!-- end hero -->
 
-    <div class="container-inner mx-auto">
-      <p class="text-lg sm:text-xl">This gridsome theme was forked from my <a href="https://andremadarang.com">personal website</a>. I added more features and decided to release it as an open source project. Check it out on <a href="https://github.com/drehimself/gridsome-portfolio-starter">GitHub</a> or check out the <a href="https://www.youtube.com/watch?v=uHo6o1TNQeE">screencast I did</a>. Feel free to use it for your own site or projects! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, repellat. Dolores iure, quas perspiciatis rerum quaerat atque eum repellat autem.</p>
+    <div class="container-inner mx-auto" id="about">
+      <p class="text-lg sm:text-xl">
+        澄煦顧問（Enlightency Consulting）是一家技術顧問公司，服務對象為軟體工程師與軟體企業。
+        澄煦提供的服務包括程式語言訓練課程、軟體專案管理訓練、軟體社群營運服務等；
+        另外，還提供職涯顧問、履歷表課程...等軟性技能服務。
+        澄煦顧問，點亮你溫暖的技術職涯。
+      </p>
 
       <div class="flex justify-between items-center py-6 -mx-8">
-        <div class="w-full sm:w-1/2 px-8 py-8 sm:py-0">
-          <g-image src="../../static/mind_map.svg" alt="browser" />
-        </div>
-        <div class="w-1/2 px-8 hidden sm:flex justify-end">
-          <g-image src="../../static/code_review.svg" alt="mobile" />
-        </div>
+
       </div>
     </div>
 
     <div class="overflow-x-hidden">
       <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
-        <h2 class="font-bold mb-6" id="projects">Here are some projects I've worked on:</h2>
-
-        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
-          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
-        </div>
+        <h2 class="font-bold mb-6" id="articles">最新文章</h2>
 
         <ul class="text-lg sm:text-xl">
           <li class="checkmark mb-6">
-            <div>Project One</div>
-            <div class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab nam animi iure nemo exercitationem a, eius non culpa itaque!</div>
+            <div><a href="https://medium.com/enlightency/five-misunderstandings-about-resume-4c332fc91eb" target="_blank">寫履歷表時常踏入的五大思維誤區</a></div>
+            <div class="text-lg text-gray-600">
+              別再悶著頭寫履歷了！調整思維、掌握履歷的重點，才是成功求職的第一關！
+            </div>
           </li>
           <li class="checkmark mb-6">
-            <div>Project Two</div>
-            <div class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab nam animi iure nemo exercitationem a, eius non culpa itaque!</div>
+            <div><a href="https://medium.com/enlightency/we-are-enlightency-df7ebf45210f" target="_blank">澄煦顧問上台一鞠躬！</a></div>
+            <div class="text-lg text-gray-600">
+              橫跨社會科學與自然科學的兩個人會擦撞出什麼火花呢？
+            </div>
           </li>
-          <li class="checkmark mb-6">
-            <div>Project Three</div>
-            <div class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab nam animi iure nemo exercitationem a, eius non culpa itaque!</div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>Project Four</div>
-            <div class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab nam animi iure nemo exercitationem a, eius non culpa itaque!</div>
-          </li>
-
         </ul>
+
+        <div class="flex justify-between items-center py-6 -mx-8">
+          <div class="w-full sm:w-1/2 px-8 py-8 sm:py-0">
+            <g-image src="../../static/mind_map.svg" alt="browser" />
+          </div>
+          <div class="w-1/2 px-8 hidden sm:flex justify-end">
+            <g-image src="../../static/code_review.svg" alt="mobile" />
+          </div>
+        </div>
+
       </div> <!-- end projects -->
-    </div>
-
-    <div class="overflow-x-hidden border-gray-200 border-b">
-      <div class="get-to-know-me container-inner mx-auto text-xl pb-16 relative">
-        <h2 class="font-bold mb-6" id="about">Get to know me:</h2>
-
-        <div class="absolute left-0" style="top: 50px; transform: translateX(-100%)">
-          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
-        </div>
-
-        <div class="flex flex-col sm:flex-row justify-between items-center mb-16">
-          <div><g-image src="../../static/avatar.jpg" alt="avatar" class="w-32 h-32 rounded-full mb-8 lg:mb-0" /></div>
-          <div class="flex-1 text-lg sm:text-xl ml-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quae architecto minus molestias iusto ducimus fuga quo quia libero dignissimos, fugiat modi assumenda debitis laboriosam.</div>
-        </div>
-
-        <div class="responsive-container">
-          <iframe class="responsive-iframe w-full h-full top-0 left-0 border-0" src="https://www.youtube.com/embed/uHo6o1TNQeE" style="border:0;" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-      </div> <!-- end get-to-know me -->
     </div>
 
     <div class="overflow-x-hidden">
       <div class="contact-me bg-gray-100 pt-16">
         <div class="container-inner mx-auto text-xl pb-4 relative">
-          <h2 class="font-bold mb-6" id="contact">Contact me:</h2>
+          <h2 class="font-bold mb-6" id="contact">聯絡我們</h2>
 
-          <div class="absolute right-0 top-0" style="transform: translate(100%) rotate(180deg)">
-            <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
-          </div>
-
-          <p class="mb-12">Static sites cannot submit forms on their own. However, services like <a href="https://www.netlify.com/docs/form-handling">Netlify Forms</a> or <a href="https://formspree.io">FormSpree</a> can help you do this without a backend.</p>
+          <p class="mb-12">
+            若您對我們的服務有興趣，歡迎填寫以下表單與我們聯絡。
+          </p>
 
           <div class="text-lg sm:text-lg mb-16">
-            <form action="#" class="mb-12">
-              <div class="flex flex-wrap mb-6 -mx-4">
-                  <div class="w-full md:w-1/2 mb-6 md:mb-0 px-4">
-                      <label class="block mb-2 text-gray-800" for="name">
-                          Name
-                      </label>
-
-                      <input type="text" name="name" id="name" placeholder="Jon Snow" class="block w-full border shadow rounded outline-none focus:border-green-700 mb-2 p-4" required>
-                  </div>
-
-                  <div class="w-full px-4 md:w-1/2">
-                      <label class="block text-gray-800 mb-2" for="email">
-                          Email Address
-                      </label>
-
-                      <input type="email" name="email" id="email" placeholder="email@example.com"  class="block w-full border shadow rounded outline-none focus:border-green-700 mb-2 p-4" required>
-                  </div>
-              </div>
-
-              <div class="w-full mb-12">
-                  <label class="block text-gray-800 mb-2" for="message">
-                      Message
-                  </label>
-
-                  <textarea id="message" rows="4" name="message" class="block w-full border shadow rounded outline-none appearance-none focus:border-green-700 mb-2 px-4 py-4" placeholder="Enter your message here." required></textarea>
-              </div>
-
-              <div class="flex justify-end w-full">
-                  <input type="submit" value="Submit" class="block bg-green-700 hover:bg-green-800 text-white text-sm font-semibold tracking-wide uppercase shadow rounded cursor-pointer px-6 py-3">
-              </div>
-          </form>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScomM034eUatpk-pLp-5b0nBm8eqLVraxSKLge3TKgfFnKZ8Q/viewform?embedded=true" width="100%" height="870" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
           </div>
         </div>
       </div> <!-- end contact-me -->
@@ -122,16 +72,18 @@
 
     <div class="newsletter bg-gray-300">
       <div class="container-inner mx-auto py-16 pb-8 text-center textl-xl">
-        <h2 class="text-2xl font-bold mb-8">Join my Newsletter</h2>
+        <h2 class="text-2xl font-bold mb-8" id="subscribe">訂閱電子報</h2>
 
         <div class="w-4/5 mx-auto mb-8">
-          <p class="mb-8 text-lg sm:text-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, voluptatum. Quam, deleniti nulla.</p>
+          <p class="mb-8 text-lg sm:text-xl">
+            想在第一時間看到我們的新文章嗎？請透過以下表單訂閱我們的電子報，未來新文章發佈時會直接送到你的信箱！
+          </p>
 
           <form action="#">
             <div class="flex flex-col sm:flex-row">
-              <input type="email" name="email" placeholder="Your email address" class="flex-1 bg-white rounded rounded-r-none px-4 py-4 leading-normal border border-r-0 shadow outline-none focus:border-green-700 z-10" required>
-              <button data-element="submit" class="flex-2 w-40 uppercase bg-green-700 text-white rounded sm:rounded-l-none text-lg py-3 px-8 tracking-wide shadow focus:outline-none hover:bg-green-800 focus:bg-green-800 z-10 w-full sm:w-auto mt-4 sm:mt-0">
-                <span>Subscribe</span>
+              <input type="email" name="email" placeholder="請輸入您的 Email" class="flex-1 bg-white rounded rounded-r-none px-4 py-4 leading-normal border border-r-0 shadow outline-none focus:border-blue-700 z-10" required>
+              <button data-element="submit" class="flex-2 w-40 uppercase bg-blue-700 text-white rounded sm:rounded-l-none text-lg py-3 px-8 tracking-wide shadow focus:outline-none hover:bg-blue-800 focus:bg-blue-800 z-10 w-full sm:w-auto mt-4 sm:mt-0">
+                <span>訂閱</span>
               </button>
             </div>
           </form>
@@ -149,7 +101,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Home'
+    title: '首頁'
   }
 }
 </script>
